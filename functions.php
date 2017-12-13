dee4143e7b9b63335e9f134901ef73ca44fc799d
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-12-13 17:54:56 +0100

    Added custom page type 'documentaries'

--- functions.php
+++ functions.php
@@ -15,7 +15,44 @@ function my_theme_enqueue_styles() {
     );
 }
 
+// Custom post type
 
-function echo_hello(){
-    echo "Hellooooo";
+function documentaries_custom_post_type(){
+    $labels = array(
+        'name' => 'Documentaries',
+        'singular_name' => 'Documentaries',
+        'add_new' => 'Add new Item',
+        'all_items' => 'All items',
+        'add_new_item' => 'Add Item',
+        'edit_item' => 'Edit Item',
+        'new_item' => 'New Item',
+        'view_item' => 'View Item',
+        'search_item' => 'Search Documentaries',
+        'not_found' => 'No items found',
+        'not_found_in_trash' => 'No items found in trash',
+        'parent_item_colon' => 'Parent Item'
+    );
+    $arguments = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'publicly_queryable' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail',
+            'revisions'
+        ),
+        'taxonomies' => array('category', 'post_tag'),
+        'menu_position' => 5,
+        'exclude_from_search' => false
+    );
+    register_post_type('documentaries', $arguments);
 }
+
+add_action('init', 'documentaries_custom_post_type');
